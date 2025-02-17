2a48822165d2e6a530ba57548a515cce37ad9463
suryansh0010 <suryanshawasthi9@gmail.com>
2025-02-18 00:38:41 +0530

    db connected

---
+++ config/db.js
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://suryansh:964840@cluster0.mtpak.mongodb.net/food-del').then(()=>console.log("DB connected"));
+}
