b2747358a51505a4428a9d4055682d6dd64b56c9
suryansh0010 <suryanshawasthi9@gmail.com>
2025-02-19 16:42:03 +0530

    routes

---
+++ routes/foodRoute.js
@@ -0,0 +1,9 @@
+import express from express
+import { addFood } from "../controllers/foodcontroller.js"
+import multer from 'multer'
+
+const foodRouter = express.Router();
+
+
+
+export default foodRouter;
