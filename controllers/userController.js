9a48df66a3262e73047131efc841e7ed68a5b202
suryansh0010 <suryanshawasthi9@gmail.com>
2025-02-25 13:24:50 +0530

    controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,9 +8,41 @@ import validator from 'validator'
 const loginUser = async (req,res) =>{
     
 }
+
+const createToken = (id) =>{
+    return jwt.sign({id})
+}
 // login user
 const registerUser = async (req,res) =>{
+    const [name,password,email]= req.body;
+    try {
+        const exists =await userModel.findOne({email});
+        if(exists){
+            return res.json({success:false,message:"user already exists"})
+        }
 
+        // validating email formate and strong passwORD
+        if(!validator.isEmail(email)){
+            return res.json({success:false,message:"please enter a valid email"})
+        }
+
+        if(password.length<8){
+            return res.json({success:false,message:"please enter a Strong Password"})
+        }
+
+        // hasing user password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword
+        })
+        const user = await newUser.save()
+    } catch (error) {
+        
+    }
 }
 
 export{loginUser,registerUser}
