ccd137d447af416e5d4bae942cb3a88c61d1248e
suryansh0010 <suryanshawasthi9@gmail.com>
2025-02-25 12:21:38 +0530

    usercontroller

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+import userModel from "../models/userModel";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import validator from 'validator'
+
+
+// login user
+const loginUser = async (req,res) =>{
+    
+}
+// login user
+const registerUser = async (req,res) =>{
+
+}
+
+export{loginUser,registerUser}
