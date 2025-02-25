25ead27fd09979eaeffefebd5ded72d6da35d713
suryansh0010 <suryanshawasthi9@gmail.com>
2025-02-25 13:25:01 +0530

    Model

--- models/userModel.js
+++ models/userModel.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
-    cartdata:{type:Object,required:{}},
+    cartdata:{type:Object,default:{}},
 },{minimize:false})
 
 const userModel =mongoose.models.user || mongoose.model("user",userSchema);
-export default userModel; 
+export default user Model; 
